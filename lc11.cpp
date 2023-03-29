19d94ea23fb96869ea4d86a081166d5e074769f8
Achuthankrishna <achkrish07@gmail.com>
2023-03-29 16:38:00 -0400

    11th Problem solves differently

--- lc11.cpp
+++ lc11.cpp
@@ -8,7 +8,7 @@ int main()
     long int area=0;
     int h=height.size()-1;
     int l=0;
-    long int high,low,dist;
+    long int high,low,dist=0;
     cout<<height[h]<<" "<<height[l]<<endl;
 
     while(h>l)
